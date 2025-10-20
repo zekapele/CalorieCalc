5521506447a06dcf598dc8accf66c18b6cc79792
jekapele <zekapele@gmail.com>
2025-10-20 17:44:08 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    std::cout << "Calorie calculator in progress...";
+}
